460a76196b23912dd9e16d795c52a829dc0c6491
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-04-01 21:32:47 -0400

    edited explanation to 1929 solution

--- easy/1929.py
+++ easy/1929.py
@@ -2,3 +2,5 @@ class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         # given a list of ints, return a concatenated array that consists of the original list with another appended to the end
         return nums + nums
+
+        # the easiest way to append one list to another is to use the '+' operator
